9fe19be7d813eba1a520a02f2ad3fc6af109bd30
Jerish PJ <jerishonline@gmail.com>
2020-07-07 15:37:11 +0100

    BankApp project

---
+++ MyBankApp/BankAccount.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+
+namespace MyBankApp
+{
+    /// <summary>
+    /// First declare the properties
+    /// Then constuctors,
+    /// Initialize the constructors using this
+    /// This is basically form fields in UI 
+    /// </summary>
+    class BankAccount
+    {
+        /// <summary>
+        /// Properties are used to enforce rules 
+        /// </summary>
+        public string Number { get; set; }
+        public string  Owner { get; set; }
+
+        public List<Transaction> allTransaction = new List<Transaction>();
+
+        //Running balance all time and rules for balance
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransaction)
+                {
+                    balance += item.Amount;
+                }
+
+                return balance;
+            }
+        }
+
+
+        private static int accountNumberSeed = 1234567890;
+
+        /// <summary>
+        /// We add constructors because we can assign a new value to the variable
+        /// var account = new BankAccount("kendra", 10000);
+
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="initialBalance"></param>
+        /// 
+
+        //To communicate between classes a bank accouunt will have lots of transactions
+        
+
+        public BankAccount(string name,decimal initialBalance,string note)
+        {
+            this.Owner = name;
+            this.Number = accountNumberSeed.ToString();
+            makeDeposit(initialBalance, DateTime.Now, note);
+            accountNumberSeed++;
+        }
+
+        /// <summary>
+        /// Methods are called verbs
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <param name="date"></param>
+        /// <param name="note"></param>
+        public void makeDeposit(decimal amount,DateTime date,string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            
+          // Constructor in Transaction class is used to create an object..
+          // Assign that entire object to a Public generic list..
+            var deposit = new Transaction(amount, date, note);
+            allTransaction.Add(deposit);
+        }
+
+        public void makeWithdrawal(decimal amount,DateTime date,string note)
+        {
+            if(amount<=0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal should not be negative");
+            }
+
+            if(Balance-amount<=0)
+            {
+                throw new InvalidOperationException("No sufficient funds for withdrawal");
+
+            }
+
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransaction.Add(withdrawal);
+
+        }
+
+        // Transaction History
+
+        public string GetTransactionHistory()
+        {
+            var str = new StringBuilder();
+            //HEADER
+            str.Append("Amount\t\tDate\t\tNote\n");
+            foreach(var item in allTransaction)
+            {
+                //ROWS
+                str.AppendLine($"{item.Amount}\t\t{item.Date.ToShortDateString()}    \t{item.Notes}");
+            }
+
+            return str.ToString();
+        }
+
+    }
+}
---
+++ MyBankApp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace MyBankApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("kendra", 10000,"InitialBalance");
+
+            Console.WriteLine($"Account Number {account.Number}  was created for  {account.Owner} with the balance  {account.Balance}");
+
+            account.makeWithdrawal(120, DateTime.Now, "Hammock");
+            Console.WriteLine(account.Balance);
+
+            account.makeDeposit(300, DateTime.Now, "Added Money");
+            Console.WriteLine(account.Balance);
+
+
+            /*
+
+            //Testing for negative balance
+
+            try
+            {
+                var bal = new BankAccount("Invalid", -55, "Invalid Balance");
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught when creating a negative balance");
+                Console.WriteLine(e.ToString());
+            }
+
+            //Testing for overdraft
+
+            try
+            {
+                account.makeWithdrawal(75000, DateTime.Now, "Overdraft Done");
+                Console.WriteLine(account.Balance);
+            }
+            catch(InvalidOperationException e)
+            {
+                Console.WriteLine("Invalid Operation");
+                Console.WriteLine(e.ToString());
+            }*/
+
+            
+            account.makeWithdrawal(300,DateTime.Now, "Xbox Game");
+            Console.WriteLine(account.Balance);
+
+            Console.WriteLine($"Account Number {account.Number} Balance {account.Balance}");
+            Console.WriteLine(account.GetTransactionHistory());
+            Console.ReadLine();
+        }
+    }
+}
